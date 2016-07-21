903128b0b49ba6df885abe4e4f741b65d4dc669f
Zuqi Chen <jokey.chen@gmail.com>
2016-07-21 17:19:15 +0800

    Refactor to class

--- basic-ruby/ruby-building-blocks/caesar_cipher.rb
+++ basic-ruby/ruby-building-blocks/caesar_cipher.rb
@@ -1,6 +1,6 @@
 # Caesar Cipher
 
-# In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift 
+# In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift
 # cipher, Caesar's code or Caesar shift, is one of the simplest and most
 # widely known encryption techniques. It is a type of substitution cipher in
 # which each letter in the plaintext is replaced by a letter some fixed
@@ -8,21 +8,22 @@
 # D would be replaced by A, E would become B, and so on. The method is named
 # after Julius Caesar, who used it in his private correspondence.
 
-# Constants
-UPPER_A = 65
-UPPER_Z = 90
-LOWER_A = 97
-LOWER_Z = 122
-ALPHABETS = 26
+class CaesarCipher
+  # Constants
+  UPPER_A = 65
+  UPPER_Z = 90
+  LOWER_A = 97
+  LOWER_Z = 122
+  ALPHABETS = 26
 
-def caesar_cipher string, factor
+  def encoder string, factor
     # convert string to array of characters
     characters = string.chars
     # check the ascii value of each character
     characters.collect! do |char|
         # char is within A-Z
-        if char.ord >= UPPER_A and char.ord <= UPPER_Z    
-            # if adding the factor results in a "overflow" 
+        if char.ord >= UPPER_A and char.ord <= UPPER_Z
+            # if adding the factor results in a "overflow"
             if char.ord + factor > UPPER_Z
                 # wrap from Z to A
                 ((char.ord + factor - UPPER_A) % ALPHABETS + UPPER_A).chr
@@ -48,4 +49,5 @@ def caesar_cipher string, factor
 
     # return the string formed by each characters in the array
     characters.join()
+  end
 end
